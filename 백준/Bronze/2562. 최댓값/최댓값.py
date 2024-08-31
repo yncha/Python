86bbadd1a1dac65815ae1dd29b3e7dddb4b44e0a
yncha <yncha1010@gmail.com>
2024-08-31 10:15:20 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -0,0 +1,12 @@
+l= []
+l.append(int(input()))
+l.append(int(input()))
+l.append(int(input()))
+l.append(int(input()))
+l.append(int(input()))
+l.append(int(input()))
+l.append(int(input()))
+l.append(int(input()))
+l.append(int(input()))
+print(max(l))
+print(l.index(max(l))+1)
